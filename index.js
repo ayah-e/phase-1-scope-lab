7d906138ea280f697cb2584de4958e77229eba82
Ayah Elsais <elsais.ayah@gmail.com>
2023-01-11 15:36:02 -0500

    last test done

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ function setBestCustomer() {
     bestCustomer = "not bob"; //we set bestCustomer to "not bob"
 };
 
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"; //we set bestCustomer to "maybe bob"
+};
 
+const leastFavoriteCustomer = "some name"
 
-
-
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "bob"; //we try to reassign the variable with a new value 
+};
