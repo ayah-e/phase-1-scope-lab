9de8ef2706c7d17f52107a5c250509d605338c3a
Ayah Elsais <elsais.ayah@gmail.com>
2023-01-11 15:29:38 -0500

    3rd test done

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // Write your solution in this file!
-var customerName = "bob"
+var customerName = "bob";
+//global variable
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+
+var bestCustomer
+//another global variable we want it to be undefined for this test
+function setBestCustomer() {
+    bestCustomer = "not bob"; //we set bestCustomer to "not bob"
+};
+
+
+
+
 
